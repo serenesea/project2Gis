a475049114bc26ba26d08070fcd4212028639b5b
markato <markato.ltd@gmail.com>
2017-01-29 19:17:11 +0200

    Add files via upload
    Temporary

---
+++ EXODUS/js/map.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var uluru = {lat: 54.223143, lng: 28.512782};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
